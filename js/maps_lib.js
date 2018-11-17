435f1137c3d0f212236ff5b759a6f950e9bfc3e2
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-16 17:14:11 -0800

    column name change

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -178,7 +178,7 @@
         self.whereClause = self.locationColumn + " not equal to ''";
         
         //-----custom filters-----
-        var type_column = "'type'";
+        var type_column = "INCOME_REQUIRED_TO_RENT_OR_BUY";
 
         if ( $("#rbType1").is(':checked')) self.whereClause += " AND " + type_column + "=1";
         if ( $("#rbType2").is(':checked')) self.whereClause += " AND " + type_column + "=2";
@@ -200,7 +200,7 @@
 
     MapsLib.prototype.getInfo = function (callback) {
         var self = this;
-        jQuery.ajax({
+rbtype        jQuery.ajax({
             url: 'https://www.googleapis.com/fusiontables/v2/tables/' + self.fusionTableId + '?key=' + self.googleApiKey,
             dataType: 'json'
         }).done(function (response) {
