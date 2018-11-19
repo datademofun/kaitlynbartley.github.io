cbaab3cd6072dea1b5e0609126ed153b596ed20c
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-19 14:36:42 -0800

    value slider change

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -91,7 +91,7 @@
             range: true,
             min: 57500,
             max: 1077300,
-            values: [57500, 1077300],
+            values: [0, 1077300],
             step: 5,
             slide: function (event, ui) {
                 $("#age-selected-start").html(ui.values[0]);
