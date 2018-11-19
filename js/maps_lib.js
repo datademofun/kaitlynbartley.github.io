706aacb79ff8304faa30bfcf1cc637098c69c304
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-19 14:29:25 -0800

    initialize

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -86,7 +86,7 @@
         $("#result_box").hide();
 
         //-----custom initializers-----
-        $("#rbType1").props("checked", "checked");
+        // $("#rbType1").props("checked", "checked");
         $("#age-slider").slider({
             orientation: "horizontal",
             range: true,
