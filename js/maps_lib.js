cb2779ec98fb008d51772cdcb29024e4716bb400
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-20 09:06:13 -0800

    remove left slider

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -94,7 +94,7 @@
             values: [0, 1077300],
             step: 5,
             slide: function (event, ui) {
-                $("#age-selected-start").html(ui.values[0]);
+                // $("#age-selected-start").html(ui.values[0]);
                 $("#age-selected-end").html(ui.values[1]);
             },
             stop: function(event, ui) {
