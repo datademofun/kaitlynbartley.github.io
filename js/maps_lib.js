faa1ab58ff62b2fb515a0d50760b3f7d381d7d72
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-20 09:48:29 -0800

    remove range

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -94,7 +94,7 @@
             values: [500000],
             step: 5,
             slide: function (event, ui) {
-                $("#age-selected-start").html(ui.values[0]);
+                // $("#age-selected-start").html(ui.values[0]);
                 $("#age-selected-end").html(ui.values[1]);
             },
             stop: function(event, ui) {
@@ -200,7 +200,7 @@
         if ( $("#rbType1").is(':checked')) self.whereClause += " AND " + type_column + "=1";
         if ( $("#rbType2").is(':checked')) self.whereClause += " AND " + type_column + "=2";
 
-        self.whereClause += " AND 'INCOME_REQUIRED_TO_RENT_OR_BUY' >= '" + $("#age-selected-start").html() + "'";
+        // self.whereClause += " AND 'INCOME_REQUIRED_TO_RENT_OR_BUY' >= '" + $("#age-selected-start").html() + "'";
         self.whereClause += " AND 'INCOME_REQUIRED_TO_RENT_OR_BUY' <= '" + $("#age-selected-end").html() + "'";
 
         //-----end of custom filters-----
