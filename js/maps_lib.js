c4b2c10739c573b22d05641b1aa53dbf0a6f3171
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-20 10:04:57 -0800

    slider

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -91,10 +91,10 @@
             range: false,
             min: 0,
             max: 1077300,
-            values: [0, 500000],
+            values: [500000],
             step: 5,
             slide: function (event, ui) {
-                $("#age-selected-start").html(ui.values[0]);
+                // $("#age-selected-start").html(ui.values[0]);
                 $("#age-selected-end").html(ui.values[1]);
             },
             stop: function(event, ui) {
