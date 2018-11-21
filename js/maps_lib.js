77799e5727d3551f98f9dfa12ebbb8ced0a33f3b
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-21 16:25:31 -0700

    about page

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -112,8 +112,8 @@
     //-----custom functions-----
 
     MapsLib.prototype.addCommas = function() {    
-        var number= "age-selected-end";
-        document.body.innerHTML = number.toLocaleString("en");
+        var number = "age-selected-end";
+        number.toLocaleString("en");
     };
 
 
