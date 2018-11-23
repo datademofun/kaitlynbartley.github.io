2b01d333ed454d4dcc5722227751354e9c5a01ed
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-23 15:40:37 -0700

    slider

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -95,7 +95,7 @@
             step: 5,
             slide: function (event, ui) {
                 // $("#age-selected-start").html(ui.values[0]);
-                $("#selectedincome").html(ui.values[0]);
+                $("#selectedincome").html(self.addCommas(ui.values[0]));
             },
             stop: function(event, ui) {
               self.doSearch();
@@ -341,16 +341,21 @@
     };
 
 
+    // MapsLib.prototype.addCommas = function (nStr) {
+    //     nStr += '';
+    //     x = nStr.split('.');
+    //     x1 = x[0];
+    //     x2 = x.length > 1 ? '.' + x[1] : '';
+    //     var rgx = /(\d+)(\d{3})/;
+    //     while (rgx.test(x1)) {
+    //         x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    //     }
+    //     return x1 + x2;
+    // };
+
     MapsLib.prototype.addCommas = function (nStr) {
-        nStr += '';
-        x = nStr.split('.');
-        x1 = x[0];
-        x2 = x.length > 1 ? '.' + x[1] : '';
-        var rgx = /(\d+)(\d{3})/;
-        while (rgx.test(x1)) {
-            x1 = x1.replace(rgx, '$1' + ',' + '$2');
-        }
-        return x1 + x2;
+        var nStr = '';
+        console.log(nStr.toLocaleString());
     };
 
     // maintains map centerpoint for responsive design
