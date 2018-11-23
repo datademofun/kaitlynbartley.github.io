11b6272f4f1192450c4012a5f1c411a3051405df
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-23 10:14:58 -0700

    slider

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -95,7 +95,8 @@
             step: 5,
             slide: function (event, ui) {
                 // $("#age-selected-start").html(ui.values[0]);
-                $("#selectedincome").html(ui.values[0]);
+                // $("#selectedincome").html(ui.values[0]);
+                $("#selected").html(self.addCommas(ui.values[0]);
             },
             stop: function(event, ui) {
               self.doSearch();
