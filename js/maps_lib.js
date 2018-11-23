eebc6411a049fa544ed9da76cc2111c3b9e92ce7
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-23 15:46:18 -0700

    slider

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -95,7 +95,7 @@
             step: 5,
             slide: function (event, ui) {
                 // $("#age-selected-start").html(ui.values[0]);
-                $("#selectedincome").html(self.addCommas(ui.values[0]));
+                $("#selectedincome").html(ui.values[0]);
             },
             stop: function(event, ui) {
               self.doSearch();
@@ -335,28 +335,24 @@
             name = self.recordName;
         }
         $("#result_box").fadeOut(function () {
-            $("#result_count").html(self.addCommas(numRows) + " " + name + " found");
+            $("#result_count").html(self.addCommas(numRows)) + " " + name + " found");
         });
         $("#result_box").fadeIn();
     };
 
 
-    // MapsLib.prototype.addCommas = function (nStr) {
-    //     nStr += '';
-    //     x = nStr.split('.');
-    //     x1 = x[0];
-    //     x2 = x.length > 1 ? '.' + x[1] : '';
-    //     var rgx = /(\d+)(\d{3})/;
-    //     while (rgx.test(x1)) {
-    //         x1 = x1.replace(rgx, '$1' + ',' + '$2');
-    //     }
-    //     return x1 + x2;
-    // };
-
     MapsLib.prototype.addCommas = function (nStr) {
-        var nStr = '';
-        console.log(nStr.toLocaleString('en', {useGrouping:true}));
+        nStr += '';
+        x = nStr.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
     };
+
 
     // maintains map centerpoint for responsive design
     MapsLib.prototype.calculateCenter = function () {
