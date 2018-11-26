6134e0040c7db35777aa3e6ce7ac9ff6727bc497
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-26 10:09:12 -0800

    about page

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -109,7 +109,7 @@
             max: 1077300,
             values: [500000],
             step: 5,
-            format: { format: '##.0', locale: 'en'},
+            formatter: { format: '##.0', locale: 'en'},
             slide: function (event, ui) {
                 // $("#age-selected-start").html(ui.values[0]);
                 $("#selectedincome").html(ui.values[0]);
