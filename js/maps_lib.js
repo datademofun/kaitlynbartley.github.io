3f114265b34cc6212ac91a3fcc88810af256df47
Kaitlyn Landgraf <kaitlynl@gmail.com>
2018-11-26 11:05:16 -0800

    about page

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -111,7 +111,7 @@
             step: 5,
             slide: function (event, ui) {
                 // $("#age-selected-start").html(ui.values[0]);
-                $("#selectedincome").html(ui.values[0].toLocaleString('us-US'));
+                $("#selectedincome").html(ui.values[0]);
             },
             stop: function (event, ui) {
               self.doSearch();
